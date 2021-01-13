e2d8f3ac80bf7bb124588ae195eaec57340cdfd0
emniko <49527089+emniko@users.noreply.github.com>
2021-01-13 15:09:12 +0500

    Page Setup

--- BarcodePrinter/Form1.cs
+++ BarcodePrinter/Form1.cs
@@ -18,11 +18,19 @@ namespace BarcodePrinter
         }
         private void myPrintDocument2_PrintPage(System.Object sender, System.Drawing.Printing.PrintPageEventArgs e)
         {
-            Bitmap myBitmap1 = new Bitmap(pictureBox1.Width, pictureBox1.Height);
-            pictureBox1.DrawToBitmap(myBitmap1, new Rectangle(0, 0, pictureBox1.Width, pictureBox1.Height));
-            e.Graphics.DrawImage(myBitmap1, 0, 0);
             e.PageSettings.PaperSize = new System.Drawing.Printing.PaperSize("Label", 2, 1);
-            myBitmap1.Dispose();
+            Graphics drawing = e.Graphics;
+            drawing.Clear(Color.White);
+            Brush textBrush = new SolidBrush(Color.Black);
+            Font font = new Font("Arial Narrow", 14, FontStyle.Bold);
+            Font fontBig = new Font("Arial Narrow", 36, FontStyle.Bold);
+            drawing.DrawString("SHELF:", font, textBrush, 10, 15);
+            drawing.DrawString("49", fontBig, textBrush, 80, 5);
+            drawing.DrawString("SKU: 9517-28", font, textBrush, 10, 80);
+            drawing.DrawString("COUNT: 14", font, textBrush, 150, 80);
+
+            textBrush.Dispose();
+            drawing.Dispose();         
         }
 
         private void button1_Click_1(object sender, EventArgs e)
