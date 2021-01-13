6737add792a7d4a07682a4672f05e18b2468cf06
Abdul Moiz <moiza0799@gmail.com>
2021-01-13 14:25:48 +0500

    Initial

--- BarcodePrinter/Form1.cs
+++ BarcodePrinter/Form1.cs
@@ -16,5 +16,21 @@ namespace BarcodePrinter
         {
             InitializeComponent();
         }
+
+        private void myPrintDocument2_PrintPage(System.Object sender, System.Drawing.Printing.PrintPageEventArgs e)
+        {
+            Font font = new Font("Arial", 24, FontStyle.Bold);
+            Bitmap myBitmap1 = (Bitmap)DrawText(font);// new Bitmap(60, 30);
+            pictureBox1.Image = myBitmap1;
+            pictureBox1.DrawToBitmap(myBitmap1, new Rectangle(0, 0, myBitmap1.Width, myBitmap1.Height));
+            e.Graphics.DrawImage(myBitmap1, 0, 0);
+            myBitmap1.Dispose();
+        }
+        private void button1_Click_1(object sender, EventArgs e)
+        {
+            System.Drawing.Printing.PrintDocument myPrintDocument1 = new System.Drawing.Printing.PrintDocument();
+            myPrintDocument1.PrintPage += new System.Drawing.Printing.PrintPageEventHandler(myPrintDocument2_PrintPage);
+            myPrintDocument1.Print();
+        }
     }
 }
